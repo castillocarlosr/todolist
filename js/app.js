7b13547871d8f62cda10d6866801bc4225ed0ce1
Blaise Clarke <blaiserclarke@gmail.com>
2018-10-01 11:34:06 -0700

    Added a handler for adding a new Task object to the global array and resaving it to localStorage

--- js/app.js
+++ js/app.js
@@ -14,10 +14,33 @@ Task.allTasks = [];
 
 
 function buildTasks(){
-  new Task('task 1', 'task 1 description - synch', 'daily', 1);
-  new Task('task 2', 'task 2 description - synch', 'daily', 5);
-  new Task('task 3', 'task 3 description - asynch', 'toDo', 5);
+  new Task('task 1', 'task 1 description - synch', 'daily', 'now', 1);
+  new Task('task 2', 'task 2 description - synch', 'daily', 'soon', 5);
+  new Task('task 3', 'task 3 description - asynch', 'toDo', 'later', 5);
   localStorage.setItem('tasks', JSON.stringify(Task.allTasks));
+}
+
+function addTask(taskName, taskDescript, taskType, dueDate, pointValue){
+  //check for uniqueness for all tasks, regardless of type 
+  if(bTaskUnique(taskName)){
+    new Task(taskName, taskDescript, taskType, dueDate, pointValue);
+    localStorage.setItem('tasks', JSON.stringify(Task.allTasks));
+  }
+  else{
+    //How do we need to handle duplicate task names?
+    console.log('Task name already in use, use something else');
+  }
+}
+
+function bTaskUnique(taskName){
+  let bResult = true;
+  for(let i = 0; i < Task.allTasks.length; i++){
+    if(Task.allTasks[i].name === taskName){
+      bResult = false;
+      break;
+    }
+  }
+  return bResult;
 }
 
 function generateTasks(){
