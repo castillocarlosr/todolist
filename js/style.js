0b2e9c9a26b182c60d40880128b11d0891000fc3
LENA EIVY <applena@gmail.com>
2018-10-01 15:10:23 -0700

    added modal to todos

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-// Get the modal
+// Get the daily modal
 var modal = document.getElementById('dailyModal');
 
 // Get the button that opens the modal
@@ -19,8 +19,35 @@ close.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the todo modal
+var todoModal = document.getElementById('todoModal');
+
+// Get the button that opens the modal
+var todoButton = document.getElementById('todoButton');
+
+// Get the <span> element that closes the modal
+var closeOne = document.getElementsByClassName('close')[1];
+
+// When the user clicks on the button, open the modal 
+todoButton.onclick = function() {
+    event.preventDefault();
+    todoModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeOne.onclick = function() {
+    todoModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == todoModal) {
+        todoModal.style.display = "none";
+    }
+}
