d4e0632283d9db6fe0505076a7f51620479db662
LENA EIVY <applena@gmail.com>
2018-10-02 10:56:47 -0700

    avatar class

--- js/avatar.js
+++ js/avatar.js
@@ -1,18 +1,19 @@
 'use strict';
 
+var avatarClass = 'Peasant';
+var currentPoints = 0;
 
 //append a p tag to the avatar id with the current class name
 var getAvatar = document.getElementById('avatar');
 var className = document.createTextNode(avatarClass);
 var getP = document.createElement('p');
-getAvatar.appendChild(getP);
 getP.appendChild(className);
-var avatarClass = 'Peasant';
+getAvatar.appendChild(getP);
 
 //append a p tag to the smallAvatar id with the current points value
 var getSmallAvatar = document.getElementById('smallAvatar');
 var pointsValue = document.createTextNode(currentPoints);
 getSmallAvatar.appendChild(getP);
 getP.appendChild(pointsValue);
-var currentPoints = 0;
+
 
